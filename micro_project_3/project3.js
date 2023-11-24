7675f099dbdae7c99005b67f3b8ea1863f1e5ba9
Arun <arun36824@gmail.com>
2023-11-24 21:53:22 +0530

    basic get all done

--- micro_project_3/project3.js
+++ micro_project_3/project3.js
@@ -9,13 +9,85 @@ function getAllFoodItems(jsondata) {
   return allfoodItems;
 }
 
+function getAllVegetables(jsondata) {
+  var allVegetables = [];
+
+  jsondata.forEach((e) => {
+    if (e.category === "Vegetable") {
+      allVegetables.push([e.foodname, e.id]);
+    }
+  });
+  return allVegetables;
+}
+
+function getAllFruits(jsondata) {
+  var allFruits = [];
+
+  jsondata.forEach((e) => {
+    if (e.category === "Fruit") {
+      allFruits.push([e.foodname, e.id]);
+    }
+  });
+
+  return allFruits;
+}
+
+function getAllProtine(jsondata) {
+  var getProtines = [];
+
+  jsondata.forEach((e) => {
+    if (e.category === "Protein") {
+      getProtines.push([e.foodname, e.id]);
+    }
+  });
+  return getProtines;
+}
+
+function getAllNuts(jsondata) {
+  var getNuts = [];
+
+  jsondata.forEach((e) => {
+    if (e.category === "Nuts") {
+      getNuts.push([e.foodname, e.id]);
+    }
+  });
+  return getNuts;
+}
+
+function getAllGrains(jsondata) {
+  var getGrains = [];
+
+  jsondata.forEach((e) => {
+    if (e.category === "Grain") {
+      getGrains.push([e.foodname, e.id]);
+    }
+  });
+  return getGrains;
+}
+function getAllNuts(jsondata) {
+  var getNuts = [];
+
+  jsondata.forEach((e) => {
+    if (e.category === "Nuts") {
+      getNuts.push([e.foodname, e.id]);
+    }
+  });
+  return getNuts;
+}
 function runner() {
   var ans;
-  ans = getAllFoodItems(data);
+  //   ans = getAllFoodItems(data);
+  //   ans = getAllVegetables(data);
+  // ans = getAllFruits(data);
+  // ans = getAllProtine(data);
+  // ans = getAllNuts(data);
+  // ans = getAllGrains(data);
+  
 
   ans.forEach((element) => {
     console.log(element);
   });
+  //   console.log(ans.length);
 }
 
 runner();
