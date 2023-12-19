b5f6b9dbc01d4f31f81e2a369f0c45b1a7b36abb
Arun <arun36824@gmail.com>
2023-12-19 13:17:08 +0530

    rules popup wip

--- rock_paper_scissors/JS/index.js
+++ rock_paper_scissors/JS/index.js
@@ -92,4 +92,12 @@ function refreshScore() {
     localStorage.getItem("computerWins");
 }
 
+function openPopup() {
+  document.getElementById("popup").style.display = "flex";
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
+
 window.onload = pageRunner();
